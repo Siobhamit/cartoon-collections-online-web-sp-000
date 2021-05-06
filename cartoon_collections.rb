86ab89e1083e34dc85278b381ac3cb1863dbf67c
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-06 01:56:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,10 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index{|dwarf, index| puts (index + 1).to_s.concat". #{dwarf}"}
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map{|call| call[0].upcase call.concat"!"}
+def summon_captain_planet(planeteer_calls = ["earth", "wind", "fire", "water", "heart"])
+  re = []
+  planeteer_calls.map{|call| re << call[0].upcase.concat"!"}
+  return re
 end
 
 def long_planeteer_calls# code an argument here
